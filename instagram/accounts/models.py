93b50e1e7a2b0cd04e18b46f6a952f988e712af5
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-08-01 22:55:37 +0500

    Добавиль модель пользователя

--- instagram/accounts/models.py
+++ instagram/accounts/models.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True, null=False, blank=False)
+    avatar = models.ImageField(upload_to='avatars', verbose_name='Аватар')
+    description = models.TextField(max_length=1000, null=True, blank=True, verbose_name='О себе')
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female')], blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __repr__(self) -> str:
+        return self.user.get_full_name()
