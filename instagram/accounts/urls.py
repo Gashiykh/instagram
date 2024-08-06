7af02e0744755b3bd18007471b85e66913032e6a
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-08-06 15:28:30 +0500

    Url путь до страницы логина

--- instagram/accounts/urls.py
+++ instagram/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as django_views
 
 from accounts import views
 
 urlpatterns = [
+    path ('login', django_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('register', views.RegisterView.as_view(), name='register')
+
 ]
