74540c6b9ca46a6b676548a2de839bdc305c3cb8
Конзуровский Александр <lianth509@gmail.com>
2024-08-06 14:47:07 +0500

    Реализован набросок детального просмотра поста.

--- instagram/webapp/views/__init__.py
+++ instagram/webapp/views/__init__.py
@@ -2,4 +2,4 @@ from .index import IndexListView
 from .search import UserSearchView
 from .user_profile_view import UserProfileView
 from .follow import FollowView
-from .post import PostCreateView, PostListView
+from .post import PostView, PostCreateView, PostListView
