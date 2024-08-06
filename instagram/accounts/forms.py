8ac299d9f51a12281641b6fadc06548ee97126c4
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-08-06 19:17:11 +0500

    Добавил форму для Логин

--- instagram/accounts/forms.py
+++ instagram/accounts/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django import forms
 
 
 class MyUserCreationForm(UserCreationForm):
@@ -10,3 +11,28 @@ class MyUserCreationForm(UserCreationForm):
             'password1', 'password2', 'first_name',
             'description', 'phone_number', 'gender',
             ]
+
+
+class LoginForm(forms.Form):
+    username_or_email = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username_or_email = cleaned_data.get('username_or_email')
+        password = cleaned_data.get('password')
+
+        if username_or_email and password:
+            user = authenticate(request=self.request, username=username_or_email, password=password)
+            if user is None:
+                try:
+                    user = authenticate(request=self.request, email=username_or_email, password=password)
+                except Exception as e:
+                    user = None
+                if user is None:
+                    raise forms.ValidationError('Неправильный логин или пароль')
+            self.user = user
+        return cleaned_data
+
+    def get_user(self):
+        return getattr(self, 'user', None)
