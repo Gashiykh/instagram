7446178b748c99def9bf21dd0950ae2023720fb5
Конзуровский Александр <lianth509@gmail.com>
2024-08-06 21:53:52 +0500

    Исправил ошибку детального просмотра.

--- instagram/accounts/urls.py
+++ instagram/accounts/urls.py
@@ -3,8 +3,9 @@ from django.contrib.auth import views as django_views
 
 from accounts import views
 
+
 urlpatterns = [
-    path ('login',  views.login_view, name='login'),
-    path('register', views.RegisterView.as_view(), name='register'),
-    path('logout', django_views.LogoutView.as_view(), name='logout')
+    path('login/', views.login_view, name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('logout/', django_views.LogoutView.as_view(), name='logout')
 ]
