146ae9aebdfbc8f8a0d833b484647512d96b44a2
Конзуровский Александр <lianth509@gmail.com>
2024-08-07 14:12:48 +0500

    Слияние и правка профиля.

--- instagram/webapp/urls.py
+++ instagram/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 from .views import IndexListView, UserSearchView, FollowView
-from .views import PostCreateView, PostView, PostListView, LikeView, CommentCreateView
-from .views import UserProfileView
+from .views import PostCreateView, PostView, PostListView, LikeView
+from .views import UserProfileView, CommentCreateView
+
 
 urlpatterns = [
     path('', IndexListView.as_view(), name='home'),
@@ -15,8 +16,9 @@ urlpatterns = [
     path('follow/<int:user_id>/', FollowView.as_view(), name='follow'),
     path('add_post/', PostCreateView.as_view(), name='add_post'),
     path('posts/', PostListView.as_view(), name='posts'),
-    path('posts/<int:post_id>/', PostView.as_view(), name='post'),
+    path('post/<int:post_id>/', PostView.as_view(), name='post'),
     path('like/<int:post_id>/', LikeView.as_view(), name='like-post'),
-    path('posts/<int:post_id>/comment/', CommentCreateView.as_view(), name='add_comment'),
-
+    path('posts/<int:post_id>/comment/',
+         CommentCreateView.as_view(),
+         name='add_comment'),
 ]
