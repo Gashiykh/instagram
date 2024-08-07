888da580e52a1558cf8f75da1b6ba4299273504b
Кабидоллаев Гашиых <gashiykh@bk.ru>
2024-08-07 17:41:57 +0500

    Добавил url путь удаления Поста

--- instagram/webapp/urls.py
+++ instagram/webapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import IndexListView, UserSearchView, FollowView
 
-from .views import PostCreateView, PostView, LikeView, CommentCreateView
+from .views import PostCreateView, PostView, LikeView, CommentCreateView, PostDeleteView
 from .views import UserProfileView
 
 
@@ -17,6 +17,7 @@ urlpatterns = [
     path('follow/<int:user_id>/', FollowView.as_view(), name='follow'),
     path('add_post/', PostCreateView.as_view(), name='add_post'),
     path('post/<int:post_id>/', PostView.as_view(), name='post'),
+    path('post/<int:post_id>/delete', PostDeleteView.as_view(), name='post_delete'),
     path('like/<int:post_id>/', LikeView.as_view(), name='like-post'),
     path('posts/<int:post_id>/comment/',
          CommentCreateView.as_view(),
